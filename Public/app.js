f26cac5416d51ad78b3b46d9826ac9c8d94e0d28
Munawarirfan <imunawarali0@gmail.com>
2024-01-05 21:36:18 -0800

    feat:assignment Chanpter 34

---
+++ Public/app.js
@@ -0,0 +1,296 @@
+// alert("Munawar ");
+// alert("Ali");
+// alert("ashmunawar60@gmail.com");
+// alert("09881");
+// alert("09881");
+
+// // Chapter 2
+// var letter = "camelCase";
+// var b = "";
+// var b = "stringName";
+
+// var teamname = "Pakistan";
+// alert(teamname);
+// var bestMan = "Muhammad";
+
+// Chapter 3
+// var caseQty = 144;
+// var num = 9;
+// var sumOfTwo = caseQty + num;
+// console.log(sumOfTwo);
+// var sum = 2 + 2;
+// console.log(sum);
+
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+// console.log(orderTotal);
+
+// var num = 12;
+// var secondNum = num + 1;
+// console.log(secondNum);
+
+// Chap 4
+// var productCost = 3.45;
+// var nameOfBand;
+// var personsInRoom = 17;
+// var myName = "munawar Ali";
+
+// var 10person
+// var 10person
+// var pe!rson
+// var 10person
+// var -person
+
+// // Chap5
+// var num = 20 % 6;
+// console.log(num);
+// // The modulus operator ( % ) gives the dividend reminder
+// var largeNum = 1000 * 2000;
+// console.log(largeNum);
+
+// var firstNum = 10;
+// var secondNum = 6;
+// console.log(firstNum - secondNum);
+
+// var dividend = 20 % 6;
+// console.log(dividend);
+
+// var multiplication = 22 * 2;
+// alert(multiplication);
+
+// Chap 6
+// var x = ++x;
+// var x = --100;
+
+// The value of y will be 51
+// The value of z will be 49
+
+// var Num = 10;
+// var newNum = --Num;
+// console.log(minus);
+
+// var x = 10;
+// var Num = ++x;
+// console.log(Num);
+
+// var x = 17;
+// var increment = ++x;
+// alert(increment);
+
+// Chap 7
+// calculatedNum = 2 + 2 * 6;
+// console.log(calculatedNum);
+// // The value of calculatedNum is 14
+
+// calculatedNum = (2 + 2) * 6;
+// console.log(calculatedNum);
+// // The value of calculatedNum is 24
+
+// calculatedNum = (2 + 2) * (4 + 2);
+// console.log(calculatedNum);
+// // The value of calculatedNum is 24
+
+// calculatedNum = (2 + 2) * 4 + 2;
+// console.log(calculatedNum);
+// // The value of calculatedNum is 18
+
+// var calculatedNum = (2 + 2 + 10) * 4;
+// console.log(calculatedNum);
+
+// var calculatedNum = 2 * 4 + 2 + 10;
+// console.log(calculatedNum);
+
+// var calculatedNum = 4 / (2 * 4);
+// console.log(calculatedNum);
+
+// Chap 8
+// var num = "2" + "2";
+// console.log(num);
+// // The value of num is 22
+
+// message = "Hello," + "Dolly";
+// console.log(message);
+// // The value of message is Hello, Dolly
+
+// alert("33" + 3);
+// // The message displayed in the alert box is 333
+
+// message = "Pakistan" + " Zindabad";
+// alert(message);
+
+// var concatenate = "33" + 3;
+// console.log(concatenate);
+
+// var a = "Karachi & ";
+// var b = "Islamabad";
+// var concatenate = a + b;
+// console.log(concatenate);
+
+// Chap 9
+// var firstName = prompt("Enter first name");
+// console.log(firstName);
+
+// var countryName = prompt("Enter Country name", "China");
+// console.log(countryName);
+
+// var yourName = prompt("Enter Your Name");
+
+// var countryName = prompt("Enter Country name", "China");
+
+// var a = "Munawar";
+// var b = "Ali";
+// var response = prompt(a, b);
+
+// var a = "Munawar";
+// var b = "Ali";
+// var response = prompt(a, b);
+// alert(response);
+
+// Chap 10
+// var city = "Karachi";
+// if ((city = "Karachi")) {
+//   console.log("The City OF Lights");
+// }
+
+// var x = "Motorola";
+// var y = "Motorola";
+// var z = "Name";
+// if (x === y) {
+//   alert("Enter value of z");
+// }
+
+// var zipCode = prompt("Enter Zip Code");
+// if (zipCode === "10010") {
+//   alert("Karachi");
+// } else {
+//   alert("Please write correct city code");
+// }
+
+// var x = 1;
+// if (x === 1) {
+//   x = 2;
+// }
+// console.log(x);
+
+// Chap 11
+// var num1 = 40;
+// var num2 = 19;
+// if (num1 >= num2) {
+//   console.log("number 1 is greater than number 2");
+// }
+
+// var num1 = 40;
+// var num2 = 19;
+
+// if (num1 != num2) {
+//   num1 = num2;
+// }
+// console.log(num1);
+
+// var num1 = 40;
+// var num2 = 19;
+
+// if (num1 != num2) {
+//   alert("Congratulations");
+// }
+// var namePrompt = prompt("Enter your name");
+// if (namePrompt != "Munawar") {
+//   alert("No Match");
+// }
+
+// Chap 15
+// var empty = [];
+// var city = ["Karachi"];
+
+// var alphabet = ["h", "i", "j", "k"];
+// alert(alphabet[2]);
+
+// var alphabet = ["h", "i", "j", "k", "l", "m", "n", "o"];
+// console.log(alphabet);
+// // Array length is 8;
+
+// var myArray = ["firstElement"];
+// myArray[1] = "secondElement";
+// alert(myArray[1]);
+
+// Chap 16
+// var myArray = ["Munawar"];
+// myArray.push("Ali");
+// alert(myArray[myArray.length - 1]);
+
+// var Alphabet = ["h", "i", "j", "k"];
+// Alphabet.pop();
+
+// var Alphabet = ["h", "i", "j", "k"];
+// Alphabet.push(07);
+
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+// sizes.shift();
+// console.log(sizes);
+
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+// var indexOfM = sizes.indexOf("M");
+// sizes.splice(indexOfM + 1, 0, "L");
+// console.log(sizes);
+
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+// var regSizes = sizes.slice(0, 3);
+// console.log(regSizes);
+
+// var pets = ["dog", "cat", "ox", "duck", "frog"];
+// var indexOfDog = pets.indexOf("dog");
+// pets.splice(indexOfDog + 1, 0, "parrot", "fish");
+// pets.splice(indexOfDog + 2, 3);
+// console.log(pets);
+
+// var pets = ["dog", "cat", "ox", "duck", "frog", "flea"];
+// var indexOfDuck = pets.indexOf("duck");
+// var indexOfFrog = pets.indexOf("frog");
+// var reducedPets = pets.slice(indexOfDuck, indexOfFrog + 1);
+// console.log(reducedPets);
+
+// Chap 21
+// allLower = userInput.toLowerCase();
+
+// var x = "Hello World";
+// x = x.toLowerCase();
+
+// var statement = "Jawan Pakistan";
+// lowerStatement = statement.toUpperCase();
+// console.log(lowerStatement);
+
+// var stringOfVar = "Stringtolower";
+// var lowerCasing = stringOfVar.toLowerCase();
+// console.log(lowerCasing);
+
+// var cityName = "kaRacHi";
+// alert(cityName.toUpperCase());
+
+// Chap 22-25
+// var sameWords = "captain";
+// var Result = sameWords.slice(1, 3);
+// console.log(Result);
+
+// var sameWords = "captain";
+// var numberOfCharacters = sameWords.length;
+
+// var animal = "Elephant";
+// var slicedanimal = animal.slice()
+
+// Chap 26
+// var originalNumber = 12.345;
+// var roundedNumber = Math.round(originalNumber);
+// console.log(roundedNumber);
+
+// var origNum = 7.89;
+// var roundNum = Math.ceil(origNum);
+// console.log(roundNum);
+
+// var origNum = 7.89;
+// var roundNum = Math.floor(origNum);
+// console.log(roundNum);
+
+// var originalNumber = 8.56;
+// var roundedNumber = Math.round(originalNumber);
+// console.log(roundedNumber);
